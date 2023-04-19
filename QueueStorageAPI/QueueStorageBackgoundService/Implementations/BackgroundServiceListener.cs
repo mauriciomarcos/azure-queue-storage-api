7d059d1d7a470701e51b536ee751ea2edd1bce3c
Maurício Marcos <mauricio.marcos@infraclearsalecom.onmicrosoft.com>
2023-04-19 11:33:52 -0300

    Ajustado namespace

--- QueueStorageAPI/QueueStorageBackgoundService/Implementations/BackgroundServiceListener.cs
+++ QueueStorageAPI/QueueStorageBackgoundService/Implementations/BackgroundServiceListener.cs
@@ -1,24 +1,23 @@
 ﻿using Microsoft.Extensions.Hosting;
 using QueueStorageSubscriber.Interfaces;
 
-namespace QueueStorageBackgoundService.Implementations
+namespace QueueStorageBackgoundService.Implementations;
+
+public sealed class BackgroundServiceListener : IHostedService, IDisposable
 {
-    public sealed class BackgroundServiceListener : IHostedService, IDisposable
+    private readonly IQueueSubscriberManager _queueListener;
+    private readonly System.Timers.Timer _timer;
+
+    public BackgroundServiceListener(IQueueSubscriberManager queueListener,
+                                     System.Timers.Timer timer) => (_queueListener, _timer) = (queueListener, timer);
+
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
-        private readonly IQueueSubscriberManager _queueListener;
-        private readonly System.Timers.Timer _timer;
-
-        public BackgroundServiceListener(IQueueSubscriberManager queueListener,
-                                         System.Timers.Timer timer) => (_queueListener, _timer) = (queueListener, timer);
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            _timer.Elapsed += async (sender, e) => await _queueListener.DequeueMessagesAsync("user-timeout-queue", cancellationToken);
-            await Task.CompletedTask;
-        }
-
-        public async Task StopAsync(CancellationToken cancellationToken) => await Task.CompletedTask;
-
-        public void Dispose() => _timer?.Dispose();
+        _timer.Elapsed += async (sender, e) => await _queueListener.DequeueMessagesAsync("user-timeout-queue", cancellationToken);
+        await Task.CompletedTask;
     }
+
+    public async Task StopAsync(CancellationToken cancellationToken) => await Task.CompletedTask;
+
+    public void Dispose() => _timer?.Dispose();
 }
