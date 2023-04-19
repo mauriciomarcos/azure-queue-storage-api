380c20752041c0eb0c97a3c841e1eabbb155773f
Maurício Marcos <mauricio.marcos@infraclearsalecom.onmicrosoft.com>
2023-04-19 11:34:32 -0300

    Ajuste namespace 02.

--- QueueStorageAPI/QueueStorageSubscriber/Exceptions/DequeueMessageException.cs
+++ QueueStorageAPI/QueueStorageSubscriber/Exceptions/DequeueMessageException.cs
@@ -1,26 +1,25 @@
 ﻿using System.Runtime.Serialization;
 
-namespace QueueStorageSubscriber.Exceptions
+namespace QueueStorageSubscriber.Exceptions;
+
+[Serializable]
+internal sealed class DequeueMessageException : Exception
 {
-    [Serializable]
-    internal sealed class DequeueMessageException : Exception
-    {
-        public DequeueMessageException()
-        { }
+    public DequeueMessageException()
+    { }
 
-        public DequeueMessageException(string message)
-            : base(message)
-        { }
+    public DequeueMessageException(string message)
+        : base(message)
+    { }
 
-        public DequeueMessageException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        { }
+    public DequeueMessageException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    { }
 
-        public DequeueMessageException(string message, Exception innerException)
-            : base(message, innerException)
-        { }
+    public DequeueMessageException(string message, Exception innerException)
+        : base(message, innerException)
+    { }
 
-        public override void GetObjectData(SerializationInfo info, StreamingContext context) =>
-            base.GetObjectData(info, context);
-    }
+    public override void GetObjectData(SerializationInfo info, StreamingContext context) =>
+        base.GetObjectData(info, context);
 }
